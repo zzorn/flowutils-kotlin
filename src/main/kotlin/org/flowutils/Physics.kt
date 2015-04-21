3ccb168e9289837fc0d4d13c4f3364326b926d08
zzorn <zzorn@iki.fi>
2015-04-21 05:02:36 +0300

    Sketching out physics library

--- src/main/kotlin/org/flowutils/Physics.kt
+++ src/main/kotlin/org/flowutils/Physics.kt
@@ -13,18 +13,41 @@ public data class Units(val lengthExponent: Int = 0,
         return this
     }
 
+    fun minus(other: Units): Units {
+        if (this != other) throw IllegalStateException("Can not add measurements with types $this and $other")
+        return this
+    }
+
     fun times(other: Units): Units {
         return Units(lengthExponent + other.lengthExponent,
                      timeExponent + other.timeExponent,
-                massExponent + other.massExponent)
+                     massExponent + other.massExponent)
+    }
+
+    fun div(other: Units): Units {
+        return Units(lengthExponent - other.lengthExponent,
+                     timeExponent - other.timeExponent,
+                     massExponent - other.massExponent)
     }
 
     override fun toString(): String {
         var s = ""
         if (lengthExponent != 0) {
-            s += "m"
+            s += " m"
             if (lengthExponent != 1) {
                 s += "^$lengthExponent"
+            }
+        }
+        if (timeExponent != 0) {
+            s += " s"
+            if (timeExponent != 1) {
+                s += "^$timeExponent"
+            }
+        }
+        if (massExponent != 0) {
+            s += " kg"
+            if (massExponent != 1) {
+                s += "^$massExponent"
             }
         }
         return s
@@ -32,14 +55,19 @@ public data class Units(val lengthExponent: Int = 0,
 }
 
 public val m: Units = Units(lengthExponent = 1)
+public val s: Units = Units(timeExponent = 1)
 
-public fun Double.m() : Measure = Measure(this, m)
 public fun Double.times(units: Units) : Measure = Measure(this, units)
+public fun Double.times(measure: Measure) : Measure = Measure(this * measure.value, measure.units)
+public fun Int.times(units: Units) : Measure = Measure(this.toDouble(), units)
+public fun Int.times(measure: Measure) : Measure = Measure(this.toDouble() * measure.value, measure.units)
 
 public data class Measure(var value: Double, val units: Units) {
 
     fun plus(other: Measure ): Measure = Measure(value + other.value, units + other.units)
+    fun minus(other: Measure ): Measure = Measure(value - other.value, units - other.units)
     fun times(other: Measure ): Measure = Measure(value * other.value, units * other.units)
+    fun div(other: Measure ): Measure = Measure(value / other.value, units / other.units)
 
     override fun toString(): String {
         return "$value$units"
@@ -49,11 +77,13 @@ public data class Measure(var value: Double, val units: Units) {
 
 fun main(args: Array<String>) {
 
-    val dist = 5.0*m
-    val dist2 = 3.0.m()
+    val dist = 5*m
+    val dist2 = 3*m
+    val time = 1*s
 
-    val foo = dist + dist2
-    val bar = dist * dist2
+//    val foo = dist + dist2 - time
+    val foo = dist *time+ dist2 * time
+    val bar = dist * dist2 / time
 
     println("foo = ${foo}")
     println("bar = ${bar}")
